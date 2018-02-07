a43538f635db5cfa6c5d08a2eeb77b9f1403670a
wspolPP <piotrekpo.46@gmail.com>
2018-02-07 11:57:06 +0100

    Kody oddzielnie

---
+++ fiboLiteracyjnie.c
@@ -0,0 +1,43 @@
+//zadanie domowe dotyczace  Ciag Fibonacciego
+//grupaII INFORMATYKA-m?.asp Piotr Pobier?yn
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//wersja literacyjnie
+unsigned int fib_liter(int n)
+{
+	int a = 0;
+	int b = 1;
+	int c;
+
+	for(int i = 0; i < n; ++i)
+	{
+		c = a+b;
+		a = b;
+		b = c;
+	}
+	return a;
+}
+
+
+
+int main()
+{
+
+	int n;
+	char pytanie;
+
+	do
+	{
+	printf("Podaj liczbe dla ktorej zostanie wyliczony ciag : ");
+	scanf("%d",&n);
+
+	printf("Liczba Fibonaciego to %d\n", fib_liter(n));
+	printf("Generowac kolejne liczby ciagu ? T/N ");
+	scanf(" %c", &pytanie);
+	}
+	while( pytanie == 'T' || pytanie == 't');
+	return 0;
+}
--- fiboRekukurencyjnie.c
+++ fiboRekukurencyjnie.c
@@ -5,21 +5,7 @@
 #include <stdlib.h>
 
 
-//wersja literacyjnie
-//unsigned int fib_liter(int n)
-//{
-//	int a = 0;
-//	int b = 1;
-//	int c;
 
-//	for(int i = 0; i < n; ++i)
-//	{
-//		c = a+b;
-//		a = b;
-//		b = c;
-//	}
-//	return a;
-//}
 //wersja rekurencyjnie
 unsigned int fib_rekur(int n)
 {
@@ -46,7 +32,7 @@ int main()
 	printf("Podaj liczbe dla ktorej zostanie wyliczony ciag : ");
 	scanf("%d",&n);
 	                     printf("Liczba Fibonaciego to %d\n", fib_rekur(n));
-	//printf("Liczba Fibonaciego to %d\n", fib_liter(n));
+
 	printf("Generowac kolejne liczby ci¹gu ? T/N ");
 	scanf(" %c", &pytanie);
 	}
